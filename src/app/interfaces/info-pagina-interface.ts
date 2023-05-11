7b95d91e739e016934f531c4e9af2cba257bef69
Francisco <jfmc251@gmail.com>
2023-05-11 16:02:34 -0600

    Fin Seccion 4

---
+++ src/app/interfaces/info-pagina-interface.ts
@@ -0,0 +1,12 @@
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tumbrl?: string;
+  equipo_trabajo?: any[];
+}
