f2270511c1cb07c31639c9f2c4bd5b9fcbbcd219
Francisco <jfmc251@gmail.com>
2023-05-12 21:02:16 -0600

    Fin Seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,22 +8,35 @@ import { InfoPagina } from '../interfaces/info-pagina-interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
-  cargada = false
-;
+  cargada = false;
+
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) { 
 
-   //console.log('servicio de pagina listo');
+   this.cargainfo();
+   this.cargarEquipo();
 
+  }
+
+  private cargainfo() { 
    //LEER ARCHIVO JSON
-  this.http.get('assets/data/data-pagina.json')
-  .subscribe( (resp: InfoPagina) => { 
+     this.http.get('assets/data/data-pagina.json')
+   .subscribe( (resp: InfoPagina) => { 
+      this.cargada = true
+      this.info = resp;
+   });
+  }
 
-    this.cargada = true
-    this.info = resp;
-     console.log(resp);
-     
+  private cargarEquipo() {
 
-  });
+     //LEER ARCHIVO JSON
+     this.http.get('https://angular-html-89254-default-rtdb.firebaseio.com/Equipo.json')
+   .subscribe( (resp: any) => { 
+ 
+      this.equipo = resp;
+      //console.log(resp);
+   });
 
   }
 }
